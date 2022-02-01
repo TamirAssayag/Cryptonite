ce6d2d14dfa68997af92cb3bc8af1ff7e5461c1e
bonjour <tamirassayag@gmail.com>
2022-02-01 20:10:56 +0200

    added modal with data in it of the same coin

--- pages/Home/Home.js
+++ pages/Home/Home.js
@@ -1,8 +1,6 @@
-const { coins, toggledCoins } = CryptoManager;
-
 function render() {
   let cards = "";
-  coins.map((coin, index) => {
+  CryptoManager.coins.map((coin, index) => {
     const { name, symbol, id } = coin;
     cards += `
     <div class="col-md-4">
@@ -23,7 +21,7 @@ function render() {
                 </div>
             </div>
             <div class="card-footer">
-                <button type="button" class="btn btn-dark">More Info</button>
+                <button type="button" class="btn btn-dark info-btn" data-toggle="modal" data-target="#modal-${id}" data-id="${id}">More Info</button>
                 <label class="switch">
                     <input type="checkbox" id="${id}" />
                     <span class="slider round"></span>
@@ -52,10 +50,72 @@ $("input[type=checkbox]").map((index, el) => {
 });
 
 function getAllCheckedCoins() {
-  toggledCoins.forEach((coin, index) => {
+  CryptoManager.toggledCoins.forEach((coin, index) => {
     $(`input#${coin.id}`).prop("checked", true);
   });
 }
+
+$(".info-btn").each((index, el) => {
+  // add click event to el
+  $(el).click((e) => {
+    const { id } = e.target.dataset;
+    const coinData = CryptoManager.fetchCoinByID(id);
+    try {
+      coinData.then((res) => {
+        renderModal(id, res);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  });
+});
+
+const renderModal = (id, res) => {
+  $(".modal").attr("id", `modal-${id}`); // Settings modal id to modal in the DOM
+  const modalTitle = $(`.modal-header`);
+  const modalBody = $(`#modal-${id} .modal-body`);
+  const modalFooter = $(`#modal-${id} .modal-footer`);
+  $(`#modal-${id}`).modal("show");
+  modalTitle.html(`
+      <div class="d-flex align-items-center gap-1">
+          <img src="${res.image.thumb}"></img>
+          <h4 class="mb-0">${id} (${res.symbol.toUpperCase()})</h4>
+      </div>
+  `);
+  modalBody.html(`
+     <h1>
+      $${res.market_data.current_price.usd.toLocaleString()}
+    </h1>
+    <span>
+      <u>
+        Current Market Prices:
+      </u>
+    </span>
+    <ul>
+      <li>
+        <strong>USD</strong> $${res.market_data.current_price.usd?.toLocaleString()}
+      </li>
+      <li>
+        <strong>EURO</strong> €${res.market_data.current_price.eur?.toLocaleString()}
+      </li>
+      <li>
+        <strong>ILS</strong> ₪${res.market_data.current_price.ils?.toLocaleString()}
+      </li>
+    </ul>
+    <hr/>
+    <h5>
+      Description:
+    </h5>
+    <p>
+      ${
+        res.description.en.length
+          ? res.description.en
+          : "No description available"
+      }
+    </p>
+    
+  `);
+};
 
 $("document").ready(() => {
   getAllCheckedCoins();
